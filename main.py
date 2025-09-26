784bbd1fe173966cc639ef2c644c5d19d3e60fe2
Himanshu <himanshu9704@gmail.com>
2025-09-26 14:41:06 +0530

    Add logic to rename game directories by removing '_game' suffix

--- main.py
+++ main.py
@@ -15,3 +15,13 @@ def find_all_game_paths(source):
         
         break
     return game_paths
+
+
+def get_name_from_paths(paths, to_strip):
+    new_names = []
+    for path in paths:
+        _, dir_name = os.path.split(path)
+        new_dir_name = dir_name.replace(to_strip, "")
+        new_names.append(new_dir_name)
+
+    return new_names
