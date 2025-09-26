99139abb20f8e9d4ed5a7cbddb9c4f05cd639b88
Himanshu <himanshu9704@gmail.com>
2025-09-26 14:33:30 +0530

    Implement function to find all game directories

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-# Entry point for Automated Game Pipeline
+import os
+
+
+GAME_DIR_PATTERN = "game"
+
+
+def find_all_game_paths(source):
+    game_paths = []
+
+    for root, dirs, files in os.walk(source):
+        for directory in dirs:
+            if GAME_DIR_PATTERN in directory.lower():
+                path = os.path.join(source, directory)
+                game_paths.append(path)
+        
+        break
+    return game_paths
